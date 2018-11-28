a904fe55ae21dc05d2c0842e8e42834bdc798e5c
sensei-blockchain <blockchaindev02@gmail.com>
2018-11-28 20:42:17 +0530

    Adds: envConfig

---
+++ config/envConfig.js
@@ -0,0 +1,37 @@
+const convict = require('convict');
+
+const schema = {
+  mnemonic: {
+    doc: 'Mnemonic',
+    format: String,
+    default: 'spend victory pony absurd uphold tenant casual media fork hollow champion stereo tip oppose field',
+    env: 'MNEMONIC',
+    arg: 'mnemonic',
+  },
+  derivation_path: {
+    doc: 'Path for which xpriv, xpub, public key, private key and address will be derived',
+    format: String,
+    default: 'm/0',
+    env: 'DERIVATION_PATH',
+    arg: 'path',
+  },
+  coin_symbol: {
+    doc: 'Symbol of coin in upper case',
+    format: ['BTC', 'LTC', 'DASH', 'ETH', 'ETC', 'DOGE', 'BCH'],
+    default: 'BTC',
+    env: 'COIN_SYMBOL',
+    arg: 'coin',
+  },
+  blockchain_environment: {
+    doc: 'Blockchain environment configuration',
+    format: ['livenet', 'testnet'],
+    default: 'testnet',
+    env: 'BLOCKCHAIN_ENVIRONMENT',
+    arg: 'environment',
+  },
+};
+
+const envConfig = convict(schema);
+envConfig.validate({ allowed: 'strict' });
+
+module.exports = envConfig;
